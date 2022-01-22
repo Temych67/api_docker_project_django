090f9ca2c89c67369345284922ce75f6340597c1
Artem <voloshko9361@gmail.com>
2022-01-22 15:35:55 +0200

    added new host name

--- api_docker_project/settings.py
+++ api_docker_project/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['demotestsiteondjango.herokuapp.com', 'localhost']
 
 
 # Application definition
