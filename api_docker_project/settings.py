d7e0a60d8ed9b7ac01fc905e30c20210afc18c67
Artem <voloshko9361@gmail.com>
2022-01-22 15:41:08 +0200

    disable DEBUG

--- api_docker_project/settings.py
+++ api_docker_project/settings.py
@@ -11,7 +11,7 @@ import django_heroku
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['demotestsiteondjango.herokuapp.com', 'localhost']
 
