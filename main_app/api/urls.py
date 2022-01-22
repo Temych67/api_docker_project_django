58861e02e119a67f7b44f1c3fd371167b6395f3f
Artem <voloshko9361@gmail.com>
2022-01-22 15:52:35 +0200

    updated files

--- main_app/api/urls.py
+++ main_app/api/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path("post_view/<title>", api_detail_post_view, name="detail_post"),
     path("post_view/<title>/update", api_update_post_view, name="update"),
     path("post_view/<title>/delete", api_delete_post_view, name="delete"),
-    path("post_view/create", api_create_post_view, name="create"),
+    path("post/create", api_create_post_view, name="create"),
     # comment url
     path("all_comments/<title>", ApiAllCommentsView.as_view(), name="detail_all_comments"),
     path(
